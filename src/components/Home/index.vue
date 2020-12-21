f5762ea878aa52a8182efb6c52702d04d2a19061
weiweibabo <weiweibabo@gmail.com>
2020-12-21 16:00:07 +0800

    feat(issue05)workshop&fixbtn完成(#05)

--- src/components/Home/index.vue
+++ src/components/Home/index.vue
@@ -25,6 +25,10 @@
           </div>
         </div>
       </div>
+      <!-- product-info-modal -->
+      <!-- :用 props方法傳遞 value -->
+      <product-modal v-model="showProductBool" :rowData="rowData" />
+
       <!-- blog -->
       <div class="title">
         <span>BLOG</span>
@@ -34,8 +38,55 @@
     <div class="blog-area">
       <blog-swiper />
     </div>
-    <!-- :用 props方法傳遞 value -->
-    <product-modal v-model="showProductBool" :rowData="rowData" />
+
+    <!-- workshop  -->
+    <div class="main-area workshop-area">
+      <div class="box-1 ">
+        <div class="text">
+          <div class="text-area">
+            <p>HAVE FUN</p>
+            <p>WITH</p>
+            <p>SUCCULENTS</p>
+          </div>
+        </div>
+        <div class="box-circle-1"></div>
+      </div>
+      <div class="content">
+        <p class="workshop"><span>WORKSHOP</span></p>
+        <p>Our workshop allow all of your guests a chance to create their own succulent masterpieces together.</p>
+        <p>
+          Our succulent specialist brings everything your group needs to create beautiful, professional looking
+          arrangements.
+        </p>
+        <p class="web">
+          We are there to walk your group through the planting process step by step. The host provides a planting area
+          for the class and tables, then just leave the rest up to us !
+        </p>
+      </div>
+      <div class="box-2 web">
+        <div class="cactus">
+          <img :src="require('@/assets/img/home/cactus.png')" />
+          <div class="btn">
+            <p>GET STARTED</p>
+          </div>
+        </div>
+        <div class="box-circle-2"></div>
+      </div>
+    </div>
+
+    <div class="main-area workshop-pic">
+      <div class="pic-area">
+        <img :src="require('@/assets/img/home/workshop-1.jpg')" />
+        <img :src="require('@/assets/img/home/workshop-2.jpg')" />
+        <img :src="require('@/assets/img/home/workshop-3.jpg')" />
+        <img :src="require('@/assets/img/home/workshop-4.jpg')" />
+      </div>
+    </div>
+
+    <!-- fix button -->
+    <div class="fix-btn" @click="scrollToTop()">
+      <img :src="require('@/assets/img/arrow-up-btn/Arrow-gray.svg')" />
+    </div>
   </div>
 </template>
 
@@ -69,7 +120,13 @@ export default {
       ],
     };
   },
+
+  mounted() {
+    window.addEventListener('scroll', this.yyy, true);
+  },
+
   methods: {
+    // modal
     openProduct(item) {
       const vm = this;
 
@@ -77,6 +134,39 @@ export default {
       vm.rowData = item.data;
       // 如果 true就打開 modal, false就關閉
       vm.showProductBool = true;
+    },
+
+    // scrollTop
+    yyy() {
+      let scrollTop = document.documentElement.scrollTop;
+      // console.log(scrollTop);
+
+      // workshop-text(HAVE FUN...)
+
+      if (scrollTop > 1830) {
+        document.querySelector('.text').classList.add('move-up');
+      } else {
+        document.querySelector('.text').classList.remove('move-up');
+      }
+
+      // workshop-cactus&btn
+      if (scrollTop > 2130) {
+        document.querySelector('.cactus').classList.add('move-up');
+      } else {
+        document.querySelector('.cactus').classList.remove('move-up');
+      }
+
+      // fixed-btn arrow up
+      if (scrollTop > 160) {
+        document.querySelector('.fix-btn').classList.add('show');
+      } else {
+        document.querySelector('.fix-btn').classList.remove('show');
+      }
+    },
+
+    // 返回最上面
+    scrollToTop() {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
     },
   },
 };
@@ -156,6 +246,148 @@ export default {
   cursor: grab;
 }
 
+// workshop-area
+.workshop-area {
+  display: flex;
+
+  .box-1 {
+    position: relative;
+    width: 17.5rem;
+    height: 17.5rem;
+    border-radius: 50%;
+    background: #b0d8bf;
+    top: 3.75rem;
+
+    .text {
+      position: absolute;
+
+      .text-area {
+        font-size: 1.5rem;
+        font-weight: 600;
+        color: #006737;
+        margin-top: 4.375rem;
+        transform: translate(5rem, 6.25rem);
+        text-align: center;
+      }
+    }
+    // scroll-event
+    .text.move-up {
+      transition: 0.8s ease-out;
+      transform: translateY(-6.25rem);
+    }
+
+    .box-circle-1 {
+      position: absolute;
+      width: 16.875rem;
+      height: 16.875rem;
+      border-radius: 50%;
+      border: 0.187rem solid #006737;
+      transform: translate(2.5rem, 0.312rem);
+    }
+  }
+
+  .content {
+    font-size: 1.1rem;
+    width: 32%;
+    margin-top: 15rem;
+    margin-left: 5rem;
+
+    .workshop span {
+      font-size: 1.875rem;
+      font-weight: 600;
+      color: #006737;
+      border-bottom: 4px solid #b0d8bf;
+    }
+  }
+
+  .box-2 {
+    position: relative;
+    width: 10.62rem;
+    height: 10.62rem;
+    border-radius: 50%;
+    background: #b0d8bf;
+    top: 10rem;
+    transform: translateX(2rem);
+
+    .cactus {
+      position: absolute;
+      transition: 0.8s ease-out;
+      z-index: 2;
+      img {
+        width: 14.37rem;
+        margin-top: 9rem;
+        margin-left: 4rem;
+      }
+
+      .btn {
+        width: 8.75rem;
+        height: 3.125rem;
+        border: 0.125rem solid #006737;
+        color: #fff;
+        background: #006737;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-left: 6.5rem;
+        margin-top: 1.6rem;
+        p {
+          margin-top: 1.25rem;
+        }
+        &:hover {
+          color: #006737;
+          background: #fff;
+        }
+      }
+    }
+    // scroll-event
+    .cactus.move-up {
+      transition: 0.8s ease-out;
+      transform: translateY(-6.25rem);
+    }
+  }
+
+  .box-circle-2 {
+    position: absolute;
+    width: 15rem;
+    height: 15rem;
+    border-radius: 50%;
+    border: 0.187rem solid #006737;
+    transform: translate(8.5rem, -3rem);
+  }
+}
+
+.workshop-pic {
+  margin: 3rem auto;
+  display: flex;
+  justify-content: center;
+
+  .pic-area {
+    display: flex;
+    justify-content: center;
+
+    img {
+      width: 23%;
+      margin: 0.5rem;
+    }
+  }
+}
+
+// fix-btn arrow-up
+.fix-btn {
+  width: 2.2rem;
+  position: fixed;
+  top: 90%;
+  right: 0.8rem;
+  opacity: 0;
+  z-index: 1;
+  cursor: pointer;
+}
+.fix-btn.show {
+  transition: 0.8s;
+  opacity: 1;
+}
+
+//product-pics
 .pic-1,
 .pic-4,
 .pic-7 {
