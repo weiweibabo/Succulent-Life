f8f82e79773c3b54fca79393b46c27df011e3b1b
weiweibabo <weiweibabo@gmail.com>
2020-12-22 13:34:39 +0800

    feat(issue10)workshop完成(#10)

--- src/components/Workshop/Carousel/index.vue
+++ src/components/Workshop/Carousel/index.vue
@@ -1,14 +1,6 @@
 <template>
   <div class="carousel">
-    <b-carousel
-      id="carousel-fade"
-      style="text-shadow: 0px 0px 2px #000"
-      fade
-      indicators
-      :interval="4000"
-      img-width="1024"
-      img-height="480"
-    >
+    <b-carousel id="carousel-fade" fade indicators :interval="4000" img-width="1024" img-height="480">
       <b-carousel-slide :img-src="require('@/assets/img/workshop/workshop-8.jpg')" />
       <b-carousel-slide :img-src="require('@/assets/img/workshop/workshop-7.jpg')" />
       <b-carousel-slide :img-src="require('@/assets/img/workshop/workshop-6.jpg')" />
